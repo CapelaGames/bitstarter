94e09b43d6c474a3c459ecb5ab7084f9426ca880
CapelaGames <Andrew@capelagames.com.au>
2013-07-11 10:14:50 +0000

    Added Buffer

--- web.js
+++ web.js
@@ -2,20 +2,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) 
+app.get('/', function(request, response)
 {
-  var fs = require('fs');
-  var buffer = new buffer(); 
-  response.send(buffer.toString('utf-8', fs.readFileSync("index.html")));
+  var file = require('fs');
+  var buffer = new buffer();
+  response.send(buffer.toString('utf-8', file.readFileSync("index.html")));
 });
 
 
 
 var port = process.env.PORT || 5000;
 
-app.listen(port, function() 
+app.listen(port, function()
 {
   console.log("Listening on " + port);
 });
-
-
