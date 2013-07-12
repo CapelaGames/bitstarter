8deacb942f21194dc04f4f816bf3f11e75014d59
CapelaGames <Andrew@capelagames.com.au>
2013-07-12 04:42:07 +0000

    Part 3 change to grader.js

--- grader.js
+++ grader.js
@@ -22,6 +22,7 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -40,6 +41,11 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+
+var cheerioUrl = function(url) {
+    return cheerio.load(url);
+};
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
@@ -55,20 +61,75 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkUrl = function(url, checksfile) {
+    $ = cheerioUrl(url);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
-if(require.main == module) {
+var getURL = function(URL)
+{
+    rest.get(URL).on('complete', onCompleteUrlGrab);
+};
+
+var onCompleteUrlGrab = function(result)
+{
+    if (result instanceof Error) 
+    {
+       console.log(result.message);
+       process.exit(1);
+    } 
+    else 
+    {
+       var checkJson = checkUrl(result, program.checks);
+       var outJson = JSON.stringify(checkJson, null, 4);
+       console.log(outJson);
+    }
+
+};
+
+if(require.main == module) 
+{
     program
+        .option('-u, --url <url>', 'a url to index.html')
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+    
+
+    var argvs = JSON.stringify(process.argv, null, 4);
+
+
+    if(argvs.indexOf("-u") != -1)
+    {
+       console.log("Running URL");
+       getURL(program.url);
+
+    }
+    else
+    {
+       console.log("Running FILE");
+
+       var checkJson = checkHtmlFile(program.file, program.checks);
+
+       var outJson = JSON.stringify(checkJson, null, 4);
+       console.log(outJson);
+
+    }
+
+} 
+else 
+{
     exports.checkHtmlFile = checkHtmlFile;
 }
